b215691e0035d34ee0e1fd981a02b3cc61667719
kieth <kieth000001@gmail.com>
2024-03-10 23:23:27 +0800

    Add files via upload

---
+++ skill.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>KIETH</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,700|Righteous&display=swap" rel="stylesheet">
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+   </head>
+   <body>
+      <!-- header section start -->
+      <div class="header_section">
+         <div class="header_main">
+            <div class="mobile_menu">
+               <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                  <div class="logo_mobile"><a href="index.php"><img src="images/logo.png"></a></div>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                           <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="about.php">About</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="portfolio.php">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link " href="skill.php">Skill</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link " href="contact.php">Contact</a>
+                        </li>
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+            <div class="container-fluid">
+               <div class="logo"><a href="index.php"><img src="images/logo.png"></a></div>
+               <div class="menu_main">
+                  <ul>
+                     <li class="active"><a href="index.php">Home</a></li>
+                     <li><a href="about.php">About Me</a></li>
+                     <li><a href="portfolio.php">Portfolio</a></li>
+                     <li><a href="skill.php">Skill</a></li>
+                     <li><a href="contact.php">Contact us</a></li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="services_section layout_padding">
+         <div class="container">
+            <h1 class="services_taital">My Skills </h1>
+            <p class="services_text">Besides playing online games I also got skills relevant to what you looking for</p>
+            <div class="services_section_2">
+               <div class="row">
+                  <div class="col-md-4">
+                     <div><img src="images/img-4.png" class="services_img"></div>
+                     <div class="shet">
+                        <a> <center>HTML CSS JAVASCRIPT<br> Proficieny: 7/10<br> I dont like building websites</center></a>
+                    </div>
+                 </div>
+                  <div class="col-md-4">
+                     <div><img src="images/img-5.png" class="services_img"></div>
+                     <div class="shet">
+                        <a> <center>PHP SQL<br> Proficieny: 9/10<br> Too easy once you learn the logic</center></a>
+                    </div>              
+                  </div>
+                  <div class="col-md-4">
+                     <div><img src="images/img-6.png" class="services_img"></div>
+                     <div class="shet">
+                        <a> <center>C++<br> Proficieny: 8/10<br> My 2nd and main language</center></a>
+                    </div>                 
+                    </div>    
+               </div>
+               <br>
+               <br>
+            </div>
+            <br>
+            <div class="services_section_2">
+               <div class="row">
+                  <div class="col-md-4">
+                     <div><img src="images/img-7.png" class="services_img"></div>
+                     <br>
+                     <div class="shet">
+                        <a> <center>BASH<br> Proficieny: 4/10<br> Scripting? auto pass</center></a>
+                    </div>
+                 </div>
+                  <div class="col-md-4">
+                     <div><img src="images/img-8.png" class="services_img"></div>
+                     <br>
+                     <div class="shet">
+                        <a> <center>Drawing<br> Proficieny: 1/10<br>But I used to make hyper realism before I transfer to modern stuffs.</center></a>
+                    </div>                  </div>
+                  <div class="col-md-4">
+                     <div><img src="images/img-9.png" class="services_img"></div>
+                     <br>
+                     <div class="shet">
+                        <a> <center>3D Modeling and Sculpting<br> Proficieny: 100/10<br>I really like this modern stuff to make my game.</center></a>
+                    </div>  
+                  </div>    
+               </div>
+               <br>
+               <br>
+            </div>
+         </div>
+      </div>
+      <!-- services section end -->
+      <!-- footer section start -->
+      <div class="footer_section layout_padding">
+         <div class="container">
+            <div class="location_main">
+               <div class="call_text"><img src="images/call-icon.png"></div>
+               <div class="call_text">Call @639166739965</a></div>
+               <div class="call_text"><img src="images/mail-icon.png"></div>
+               <div class="call_text">Email @kiethdaniel00@gmail.com</a></div>
+            </div>
+            <div class="social_icon">
+               <ul>
+                  <li><a href="https://www.facebook.com/profile.php?id=100082314048619"><img src="images/fb-icon.png"></a></li>
+                  <li><a href="https://www.linkedin.com/in/kieth-daniel-de-ocampo-73875a288/"><img src="images/linkedin-icon.png"></a></li>
+               </ul>
+            </div>
+         </div>
+      </div>
+     
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>    
+   </body>
+</html>
